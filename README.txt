d65dfc56af6b10d289d98c3bc871bbc5f392a3ba
sandu <sandu.grecu88@gmail.com>
2025-04-02 20:58:19 +0300

    Am adaugat toate sortarile si citirea listei de la utilizator

---
+++ README.txt
@@ -0,0 +1,19 @@
+This is a simple python app that uses tkinter to make a visualization of sorting algorithms.
+
+The steps taken to complete the program:
+1. Creating the UI for the aplication (I didn't overcomplicate things here).
+2. Implementing some sorting algorithms in python.
+3. Implementing a function that takes a list (either from the user of hard coded in the program),
+and creates a visual representation of the list, i used something like a histogram.
+
+Explaining the steps 
+For the UI i did something very simple, for this project I didn't really care a lot for the 
+beauty of the application. 
+
+The sorting algorithms used were also some of the most common nothing too special, like bubble sort,
+selection sort, insertion sort, and merge sort.
+
+The visualization of the list was the most important part (even if it's preaty simple).
+It calculates the width and height of each element in the list and represents it as a blue 
+rectangle. Also it is called after each iteration in the sorting algorithm so we see how the algorithm
+works step by step. 
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#TO DO
+# userul sa introduca lista de la tastatura
+# mai multe setari pentru utilizator
+#UI imbunatatit
+
 import tkinter as tk
 from tkinter import ttk
 import time 
@@ -16,6 +21,7 @@ class SortVisualizer:
         self.create_widgets()
         self.list_visualization(self.listaNesortata)
 
+    #Metoda care creeaza UI-ul
     def create_widgets(self):
         # Descrierea aplicației
         descrierea = ttk.Label(self.window, text="Aplicatie de vizualizarea a algoritmilor de sortare. ")
@@ -24,6 +30,12 @@ class SortVisualizer:
         # Canvas pentru vizualizarea listei
         self.canvas = tk.Canvas(self.window, width = 350, height= 300, bg = "white")
         self.canvas.pack(pady=30)
+
+        #Afisarea numarului de iteratii
+        self.numarIteratii = tk.StringVar()
+        self.numarIteratii.set("Numar iteratii: 0")
+        numarIteratiiLabel = tk.Label(self.window, textvariable = self.numarIteratii)
+        numarIteratiiLabel.pack()
 
         # Frame pentru butoanele de sortare
         sortari = tk.Frame(self.window)
@@ -35,10 +47,10 @@ class SortVisualizer:
         selectionSortButton = tk.Button(sortari, text="Selection Sort Start", command=self.start_selection_sort)
         selectionSortButton.grid(row=0, column=1, padx=5)
 
-        insertionSortButton = tk.Button(sortari, text="Insertion Sort Start", command=self.start_bubble_sort)
+        insertionSortButton = tk.Button(sortari, text="Insertion Sort Start", command=self.start_insertion_sort)
         insertionSortButton.grid(row=0, column=2, padx=5)
 
-        mergeSortButton = tk.Button(sortari, text="Merge Sort Start", command=self.start_bubble_sort)
+        mergeSortButton = tk.Button(sortari, text="Merge Sort Start", command=self.start_merge_sort)
         mergeSortButton.grid(row=0, column=3, padx=5)
 
         # Frame pentru câmpul de introducere a delay-ului
@@ -53,9 +65,37 @@ class SortVisualizer:
         self.delayEntry.insert(0, delayImplicit)  # Setăm valoarea implicită a delay-ului
         self.delayEntry.pack(side='right', padx=10, pady=20)
 
+        # Frame pentru câmpul de introducere a listei
+        listFrame = tk.Frame(self.window)
+        listFrame.pack()
+
+        listInputLabel = tk.Label(listFrame, text="Introdu lista: ")
+        listInputLabel.pack(side='left')
+
+        self.inputList = tk.Entry(listFrame)
+        inputList = [10, 2, 4, 9, 8, 5, 7, 6, 3, 1]
+        self.inputList.pack(side='left', padx=10, pady=20)
+
+        updateListDelayButton = tk.Button(listFrame, text = "Update list and delay", command=self.updateListDelay)
+        updateListDelayButton.pack(side='right')
+
+    def updateListDelay(self):
+        try:
+            self.delay = float(self.delayEntry.get())
+        except ValueError:
+            self.delay = 0.1
+        
+        input_text = self.inputList.get()
+        try:
+            self.listaNesortata = [int(x) for x in input_text.split(",")]
+        except ValueError:
+            self.listaNesortata = [10, 2, 4, 9, 8, 5, 7, 6, 3, 1]
+
+        self.list_visualization(self.listaNesortata)
+
+    #Functia pentru vizualizarea unei liste
     def list_visualization(self, lista):
-        #Functia pentru vizualizarea unei liste
-        self.canvas.delete("all")  # Curăță canvas-ul
+        self.canvas.delete("all")  
 
         nrPatrate = len(lista)
         latimea = 350 / nrPatrate * 0.95
@@ -65,13 +105,14 @@ class SortVisualizer:
         for i, j in enumerate(lista):
             self.canvas.create_rectangle(i * latimea + (i + 1) * gap, 300 - lista[i] * inaltimeUnitate, (i + 1) * latimea + i * gap, 300, fill="blue")
 
+    #Algoritmul bubble sort 
     def bubble_sort_ascending(self, listaNesortata):
-        #Algoritmul selection sort 
         lista = listaNesortata.copy()
         self.list_visualization(lista)
         delay = float(self.delayEntry.get())
         time.sleep(delay)
 
+        iteratii = 0
         n = len(lista)
         for j in range(n - 1):
             swapped = False
@@ -79,23 +120,29 @@ class SortVisualizer:
                 if lista[i] > lista[i + 1]:
                     lista[i], lista[i + 1] = lista[i + 1], lista[i]
                     swapped = True
+
+                    #vizualizam lista si actualizam nr de iteratii
                     self.list_visualization(lista)
+                    iteratii += 1
+                    self.numarIteratii.set(f"Numar iteratii: {iteratii}")
                     time.sleep(delay)
             if not swapped:
                 break
         return lista
 
     def start_bubble_sort(self):
+        self.updateListDelay()
         #thread-ul pentru bubble sort ca sa nu blocam UI-ul
         threading.Thread(target=self.bubble_sort_ascending, args=(self.listaNesortata,), daemon=True).start()
 
+    #Algoritmul selection sort 
     def selection_sort_ascending(self, listaNesortata):
-        #Algoritmul selection sort 
         lista = listaNesortata.copy()
         self.list_visualization(lista)
         delay = float(self.delayEntry.get())
         time.sleep(delay)
 
+        iteratii = 0
         n = len(lista)
         for i in range(n-1):
             minimIndex = i
@@ -103,18 +150,105 @@ class SortVisualizer:
                 if lista[j] < lista[minimIndex]:
                     minimIndex = j
             lista[i], lista[minimIndex] = lista[minimIndex], lista[i]
+            iteratii += 1
+            self.numarIteratii.set(f"Numar iteratii: {iteratii}")
             self.list_visualization(lista)
             time.sleep(delay)
 
     def start_selection_sort(self):
+        self.updateListDelay()
         threading.Thread(target=self.selection_sort_ascending, args=(self.listaNesortata,), daemon=True).start()
 
+    def insertion_sort_ascending(self, listaNesortata):
+        lista = listaNesortata.copy()
+        self.list_visualization(lista)
+        delay = float(self.delayEntry.get())
+        time.sleep(delay)
+
+        #Insertion sort algorithm
+        iteratii = 0
+        n = len(lista)
+        for i in range(1, n):
+            key = lista[i]
+            j = i - 1
+            while j >= 0 and lista[j] > key:
+                lista[j + 1] = lista[j]
+                j -= 1
+
+                self.list_visualization(lista)  # Vizualizare pas cu pas
+                iteratii += 1
+                self.numarIteratii.set(f"Numar iteratii: {iteratii}")
+                time.sleep(delay)
+
+            lista[j + 1] = key
+            self.list_visualization(lista)  # Vizualizare pas final pentru acest pas
+            time.sleep(delay)
+
+    def start_insertion_sort(self):
+        self.updateListDelay()
+        threading.Thread(target=self.insertion_sort_ascending, args=(self.listaNesortata,), daemon=True).start()
+
+    def merge_sort_ascending(self, listaNesortata):
+        lista = listaNesortata.copy()
+        self.list_visualization(lista)
+        delay = float(self.delayEntry.get())
+        time.sleep(delay)
+
+        #Algoritmul pentru merge sort
+        iteratii = [0] 
+        def merge_sort(lista, left, right):
+            if left < right:
+                mid = (left + right) // 2
+                merge_sort(lista, left, mid)
+                merge_sort(lista, mid + 1, right)
+                merge(lista, left, mid, right)
+                self.list_visualization(lista)
+                time.sleep(delay)
+
+        def merge(lista, left, mid, right):
+            left_part = lista[left:mid + 1]
+            right_part = lista[mid + 1:right + 1]
+
+            i = j = 0
+            k = left
+
+            while i < len(left_part) and j < len(right_part):
+                if left_part[i] < right_part[j]:
+                    lista[k] = left_part[i]
+                    i += 1
+                else:
+                    lista[k] = right_part[j]
+                    j += 1
+                k += 1
+                
+                iteratii[0] += 1
+                self.numarIteratii.set(f"Numar de iteratii: {iteratii[0]}")
+
+            while i < len(left_part):
+                lista[k] = left_part[i]
+                i += 1
+                k += 1
+
+            while j < len(right_part):
+                lista[k] = right_part[j]
+                j += 1
+                k += 1
+
+        merge_sort(lista, 0, len(lista) - 1)
+
+    def start_merge_sort(self):
+        self.updateListDelay()
+        threading.Thread(target=self.merge_sort_ascending, args=(self.listaNesortata,), daemon=True).start()
 
 
-# Creăm fereastra și instanțiem aplicația
-window = tk.Tk()
-list = [10, 2, 4, 9, 8, 5, 7, 6, 3, 1, 2]
-app = SortVisualizer(window, list)
+def main():
+    # Creăm fereastra și instanțiem aplicația
+    window = tk.Tk()
 
-# Rulăm fereastra
-window.mainloop()
+    list = [10, 2, 4, 9, 8, 5, 7, 6, 3, 1]
+    app = SortVisualizer(window, list)
+
+    # Rulăm fereastra
+    window.mainloop()
+
+main()
